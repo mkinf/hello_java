62422aba9984a25adc3f9fcb21cb20795860e44d
ryaneNG <ngougouremariane@gmail.com>
2022-02-18 03:50:21 +0000

    building a periodial test poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
- System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
+    System.out.println("Test poll scm periodically, works when there is a commit");
 System.out.println("Hello, World");
 }
 }
