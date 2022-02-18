349755470a94c98477c50dba09e2461fe28535e3
mkinf <mkinf@users.noreply.github.com>
2022-02-18 07:44:56 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Test poll scm periodically, works when there is a commit");
-System.out.println("Hello, World");
+    // System.out.println("Test poll scm periodically, works when there is a commit");
+System.out.println("Test for webhook");
 }
 }
